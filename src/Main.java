4ccd4f729782ed8b06189731f7cff39eb34851aa
freddycooper <almosttanonymous@gmail.com>
2024-03-19 22:39:25 +0300

    Initial commit.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Exercise 1
+        var clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+}
